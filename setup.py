83afee71453e547b5a363e4200bf7114382238f7
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2014-02-10 13:23:05 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyplot_helper',
+        version='1.0',
+        description='A collection of scripts that facilitate the creation fancy plots with matplotlib.',
+        author='Johannes Schlatow',
+        url='https://github.com/ValiValpas/pyplot_helper',
+        license="MIT",
+        packages=['pyplot_helper'],
+        install_requires=['brewer2mpl', 'numpy', 'matplotlib']
+        )
